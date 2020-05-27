55baf6e1293d7a6788f5d89430b5a69b1fe97e84
Eduards-Spila <eduards.spila93@gmail.com>
2020-05-27 06:04:04 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'iot-control-interface-app';
+  title = 'IoTBot Control App';
+  introText = 'This device is based on:'
+  totalAngularPackages;
+
+  ipAddress;
+  port;
+
+  speed;
+
+  constructor(private http: HttpClient) { }
+
+  setIP(ipValue) {
+    this.ipAddress = ipValue;
+  }
+  setPort(portValue) {
+    this.port = portValue;
+  }
+  setSpeed(speedValue) {
+    this.speed = speedValue;
+  }
+  goForward() {
+    this.http.get<any>('http://' + this.ipAddress + ':' + this.port + '/goForward?' + 'speed=' + this.speed).subscribe(data => {
+      this.totalAngularPackages = data.total;
+      console.log(this.totalAngularPackages);
+    })
+  }
+  rotateLeft() {
+    this.http.get<any>('http://' + this.ipAddress + ':' + this.port + '/rotateLeft?' + 'speed=' + this.speed).subscribe(data => {
+      this.totalAngularPackages = data.total;
+    })
+  }
+  rotateRight() {
+    this.http.get<any>('http://' + this.ipAddress + ':' + this.port + '/rotateRight?' + 'speed=' + this.speed).subscribe(data => {
+      this.totalAngularPackages = data.total;
+    })
+  }
+  goBackward() {
+    this.http.get<any>('http://' + this.ipAddress + ':' + this.port + '/goBackward?' + 'speed=' + this.speed).subscribe(data => {
+      this.totalAngularPackages = data.total;
+    })
+  }
+  fullStop() {
+    this.http.get<any>('http://' + this.ipAddress + ':' + this.port + '/fullStop?').subscribe(data => {
+      this.totalAngularPackages = data.total;
+    })
+  }
 }
